957f7596bea00efb799aaff6b2d7568d6003404f
victor.marit <victormarit.95@gmail.com>
2022-03-30 09:54:14 +0200

    test

--- src/test/java/MovieFactoryTest.java
+++
@@ -1,9 +0,0 @@
-import junit.framework.TestCase;
-import org.junit.Test;
-
-public class MovieFactoryTest extends TestCase{
-    @Test
-    public void testBidon() {
-        assertEquals(2,2);
-    }
-}
